49f7924b6e1ffde605f2517a8b506887bdecae6b
Nick Fanelli <nickfanelli5@gmail.com>
2023-07-10 22:28:59 -0400

    feat: add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
+import { createContext, useState } from 'react'
 import './App.css'
 
+type ThemeContext = {
+    theme: string,
+    toggleTheme: Function
+}
+
+export const ThemeContext = createContext<ThemeContext>({
+    theme: "light",
+    toggleTheme: () => {}
+});
+
 function App() {
-    
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+
+    const [ theme, setTheme ] = useState<string>("dark");
+
+    const toggleTheme = () => {
+        setTheme((prev) => (prev === "light" ? "dark" : "light"));
+    }
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+
+            <div id="root" className={`theme-${theme}`}>
+
+                <h1>Hello World</h1>
+                <button>Hello World</button>
+                <br />
+                <input type="text" placeholder="Hello World" />
+
+            </div>
+
+        </ThemeContext.Provider>
+    )
 }
 
 export default App

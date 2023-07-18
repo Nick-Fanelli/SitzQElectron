5b7ace4e13e5e3785fe26a4325184228b3d69880
Nick Fanelli <nickfanelli5@gmail.com>
2023-07-17 22:10:15 -0400

    feat: write some custom menus

--- electron/main.ts
+++ electron/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, Menu } from 'electron'
 import path from 'node:path'
 
 // The built directory structure
@@ -14,13 +14,13 @@ process.env.DIST = path.join(__dirname, '../dist')
 process.env.PUBLIC = app.isPackaged ? process.env.DIST : path.join(process.env.DIST, '../public')
 
 
-let win: BrowserWindow | null
+let defaultWindow: BrowserWindow | null
 // 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
 const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
 
-function createWindow() {
+const createWindow = () => {
     // Where the window is created
-    win = new BrowserWindow({
+    defaultWindow = new BrowserWindow({
         icon: path.join(process.env.PUBLIC, 'electron-vite.svg'),
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
@@ -31,20 +31,126 @@ function createWindow() {
     })
 
     // Test active push message to Renderer-process.
-    win.webContents.on('did-finish-load', () => {
-        win?.webContents.send('main-process-message', (new Date).toLocaleString())
+    defaultWindow.webContents.on('did-finish-load', () => {
+        defaultWindow?.webContents.send('main-process-message', (new Date).toLocaleString())
     })
 
     if (VITE_DEV_SERVER_URL) {
-        win.loadURL(VITE_DEV_SERVER_URL)
+        defaultWindow.loadURL(VITE_DEV_SERVER_URL)
     } else {
         // win.loadFile('dist/index.html')
-        win.loadFile(path.join(process.env.DIST, 'index.html'))
+        defaultWindow.loadFile(path.join(process.env.DIST, 'index.html'))
     }
 }
 
-app.on('window-all-closed', () => {
-    win = null
+const createMenu = () => {
+
+    const isMac = process.platform === 'darwin';
+
+    const fileMenu: Electron.MenuItemConstructorOptions = {
+        label: 'File',
+        submenu: [
+            { label: 'New Workspace' },
+            { label: 'Open Workspace' },
+            { label: 'Save Workspace' },
+            { label: 'Save Workspace As...' },
+            { label: 'Close Workspace' },
+            { label: 'Import Show Control Settings' },
+            { label: 'Export Show Control Settings' },
+        ],
+    };
+
+    const editMenu: Electron.MenuItemConstructorOptions = {
+        label: 'Edit',
+        submenu: [
+            { role: 'undo' },
+            { role: 'redo' },
+            { type: 'separator' },
+            { role: 'cut' },
+            { role: 'copy' },
+            { role: 'paste' },
+            { role: 'delete' },
+            { type: 'separator' },
+            { role: 'selectAll' },
+            { label: 'Preferences' },
+        ],
+    };
+
+    const workspaceMenu: Electron.MenuItemConstructorOptions = {
+        label: 'Workspace',
+        submenu: [
+        ],
+    };
+
+    const cueMenu: Electron.MenuItemConstructorOptions = {
+        label: 'Cue',
+        submenu: [
+            { label: 'New Cue' },
+            { label: 'Duplicate Cue' },
+            { label: 'Delete Cue' },
+            { label: 'Cue Inspector' },
+            { label: 'Group Cues' },
+            { label: 'Ungroup Cues' },
+            { label: 'Rename Cue' },
+        ],
+    };
+
+    const playbackMenu: Electron.MenuItemConstructorOptions = {
+        label: 'Playback',
+        submenu: [
+        ],
+    };
+
+    const deviceMenu: Electron.MenuItemConstructorOptions = {
+        label: 'Device',
+        submenu: [
+        ],
+    };
+
+    const windowMenu: Electron.MenuItemConstructorOptions = {
+        label: 'Window',
+        submenu: [
+        ],
+    };
+
+    const helpMenu: Electron.MenuItemConstructorOptions = {
+        label: 'Help',
+        role: 'help',
+        id: 'help',
+        submenu: [
+            { label: "Check for Updates" },
+            { label: "SitzQ Manual" },
+            { type: 'separator' },
+            { label: "Report a Bug" }
+        ],
+    };
+
+    const template: Electron.MenuItemConstructorOptions[] = [
+        fileMenu,
+        editMenu,
+        workspaceMenu,
+        cueMenu,
+        playbackMenu,
+        deviceMenu,
+        windowMenu,
+        helpMenu,
+    ];
+
+    if (isMac) {
+        template.unshift({ role: 'appMenu' });
+    }
+
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    createMenu();
 })
 
-app.whenReady().then(createWindow)
+app.on('window-all-closed', () => {
+    defaultWindow = null;
+})
+

23a118e8f08a7893bff6ea1a1105e964c72f0308
Nick Fanelli <nickfanelli5@gmail.com>
2023-07-24 19:04:18 -0400

    feat: add back button from app view

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,60 @@
 import AppView from './AppView/AppView';
 
 import './App.css'
+import { createContext, useContext, useState } from 'react';
+
+export enum View {
+
+    LanderView,
+    AppView
+
+}
+
+const AppContext = createContext<{
+
+    currentView: View,
+    setCurrentView: React.Dispatch<React.SetStateAction<View>>
+
+} | null>(null);
+
+export const useAppContext = () => {
+    const context = useContext(AppContext);
+
+    if(!context) 
+        throw new Error("useAppContext must be used within a AppContextProvider");
+
+    return context;
+}
 
 const App = () => {
 
+    const [ currentView, setCurrentView ] = useState<View>(View.AppView);
+
+    let view: any = null;
+
+    switch(currentView) {
+
+    case View.LanderView:
+        view = <button onClick={() => {
+            setCurrentView(View.AppView)
+        }}>Change Views</button>
+        break;
+
+    case View.AppView:
+        view = <AppView />;
+        break;
+
+    default:
+        break;
+
+    }
+
     return (
-        <section id="app">
-            <AppView />
-        </section>
+        <AppContext.Provider value={{ currentView, setCurrentView }}>
+            <section id="app">
+                {view}
+            </section>
+        </AppContext.Provider>
     )
 }
 
--- src/AppView/Components/HeaderComponent.tsx
+++ src/AppView/Components/HeaderComponent.tsx
@@ -5,10 +5,21 @@ import { faGear } from '@fortawesome/free-solid-svg-icons'
 
 import './HeaderComponent.css'
 import { useState } from 'react'
+import { View, useAppContext } from '../../App'
 
 const Header = () => {
 
     const [ projectName, setProjectName ] = useState<string>("Development Project");
+    
+    const appContext = useAppContext();
+
+    const handleGoBack = () => {
+
+        // TODO: SAVE PROJECT ETC...
+        
+        appContext.setCurrentView(View.LanderView);
+
+    }
 
     return (
         <>
@@ -18,6 +29,10 @@ const Header = () => {
             </div>
 
             <div>
+                <div className="back-arrow" onClick={handleGoBack}>
+                    <div className="arrow"></div>
+                </div>
+
                 <HiddenInputComponent className="project-name" value={projectName} setValue={setProjectName} />
             </div>
 

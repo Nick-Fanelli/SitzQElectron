8c989b531937a8b86061a9500cfd35daf7c4e8c9
Nick Fanelli <nickfanelli5@gmail.com>
2023-07-30 23:56:45 -0400

    feat: cue list add and remove

--- src/AppView/State/Store.ts
+++ src/AppView/State/Store.ts
@@ -1,15 +1,45 @@
 import { create } from 'zustand'
 
+import { Cue, CueList, UUID } from '../../Core/Cue'
+
 interface ProjectState {
 
     projectName: string
     setProjectName: (projectName: string) => void
+
+    cueList: Cue[]
+    setCueList: (cues: Cue[]) => void
+    createCue: <CueType extends Cue>(cueType: { new (uuid: UUID): CueType }) => void
+    updateCueByUUID: (uuid: UUID, updater: (prevCue: Cue) => Cue) => void
 
 }
 
 export const useProjectStore = create<ProjectState>((set) => ({
     
     projectName: "",
-    setProjectName: (projectName: string) => set({ projectName })
+    setProjectName: (projectName: string) => set({ projectName }),
+
+    cueList: [],
+    setCueList: (cues: Cue[]) => set({ cueList: cues }),
+    createCue: (cueType) => { set((state) => ({ cueList: CueList.createNewCue(state.cueList, cueType) })) },
+
+    updateCueByUUID: (uuid, updater) => {
+
+        set((state) => {
+
+            const updatedCueList = [...state.cueList];
+            const index = CueList.getIndexByUUID(updatedCueList, uuid);
+
+            if(index >= 0) {
+                updatedCueList[index] = updater(updatedCueList[index]);
+            } else {
+                console.error("Could not find cue with UUID of: ", uuid);
+            }
+
+            return { cueList: updatedCueList }
+
+        })
+
+    }
 
 }));
